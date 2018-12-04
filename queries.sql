de48a05c12cd8c888bc683c3b72184771aa1c424
constance-hackett <40133009+constance-hackett@users.noreply.github.com>
2018-12-04 17:53:05 -0500

    Non-casual Events
    Added a view for the events with non-casual dress codes along with their descriptions

--- queries.sql
+++ queries.sql
@@ -6,3 +6,9 @@ select organization_name,organization_description from Events join Organization 
 
 create view what_items_to_bring as
 select event_name, items from item-to-bring join Events using(event_id) where is_items = True;
+
+create view all_non_causal_events_and_what_to_wear as
+select temp.event_name as Events, dresscode.Type_of_dress_code as "Dress Code", dresscode.Dress_code_description as Description
+from (select event_name, Dresscode_Dress_code_id from events where Dresscode_Dress_code_id > 2) temp
+left join dresscode on temp.Dresscode_Dress_code_id = dresscode.Dress_code_id
+order by dresscode.Dress_code_description;
